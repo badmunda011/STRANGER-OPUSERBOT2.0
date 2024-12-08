f7c5e0aabe105284576a9b9a6aed487655ac8223
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-08 22:36:54 +0530

    Update console.py

--- SHUKLA/console.py
+++ SHUKLA/console.py
@@ -29,7 +29,7 @@ if os.path.exists("Internal"):
 
 
 API_ID = int(getenv("API_ID", 25742938))
-API_HASH = getenv("API_HASH", b35b715fe8dc0a58e8048988286fc5b6)
+API_HASH ="b35b715fe8dc0a58e8048988286fc5b6"
 BOT_TOKEN ="7454086236:AAEY9IfwBZqV5KTvWkqYS_GGLa6SdKcCCAc"
 STRING_SESSION ="BQGIzloAS8aDcjKd8zRdYkAXdQA1FLXOF1S2cERCELpbw5PKX95SDUH1pc8BqkMSm-KJkp2yAtOgHk4JOfjgiuoDFbAE91UCnkCpfZq9dN7HkxCXTw4gis9hpXuDW5xqSXYdxBiI8UxQEk2X430-CVhPBUshzEP3wJj4fP31gN_ffbPZcJytSoC2e1SS0vBHWkUUJITqtLYpObavXaEbFPK0e7o6OtpKnfCzlH4q19K0xY3UqJmNIYAXYkNcse1zbPRu-4n0F6Rcfcd8iIxwm7XlUt5Sf84_T4QGYgvGCjFVWO4cHs-zCHhgtJMkYTyWVoOgQyH9vcuGOBaFntjxOrKTScflRwAAAAF5XDOMAA"
 MONGO_DB_URL ="mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority"
