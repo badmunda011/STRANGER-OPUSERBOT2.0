4b53d6f6ccc8c454dece027e5cce149f802c3fb2
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-09 00:56:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
-
+import re
 from os import getenv
-from io import BytesIO
-from dotenv import load_dotenv
-from pyrogram import Client, filters
 
-# config variables
-if os.path.exists("Config.env"):
-    load_dotenv("Config.env")
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
 
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002356967761"))
