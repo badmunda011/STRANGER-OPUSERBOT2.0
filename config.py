b8fc4e2646b4bd55c8c16c24130544664edfdd4b
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-09 10:25:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,6 @@ load_dotenv()
 
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002356967761"))
 
+BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
+
 START_IMAGE_URL = getenv("START_IMAGE_URL", "https://files.catbox.moe/6v7esb.jpg")
