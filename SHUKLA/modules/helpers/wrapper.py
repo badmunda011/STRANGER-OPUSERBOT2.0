78cd498dfe2636f132bc5ca800b788c1c676a2a7
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-09 09:23:32 +0530

    Update wrapper.py

--- SHUKLA/modules/helpers/wrapper.py
+++ SHUKLA/modules/helpers/wrapper.py
@@ -12,33 +12,25 @@ def super_user_only(mystic):
         except:
             if message.outgoing:
                 return await mystic(client, message)
-            
     return wrapper
-
-
 
 def sudo_users_only(mystic):
     async def wrapper(client, message):
         try:
             if (message.from_user.is_self or
-               message.from_user.id in SUDOERS
-            ):
+               message.from_user.id in SUDOERS):
                 return await mystic(client, message)
         except:
             if (message.outgoing or
-               message.from_user.id in SUDOERS
-            ):
+               message.from_user.id in SUDOERS):
                 return await mystic(client, message)
-            
     return wrapper
-    
 
 def cb_wrapper(func):
     async def wrapper(bot, cb):
         sudousers = SUDOERS
         if (cb.from_user.id != app.me.id and
-            cb.from_user.id not in sudousers
-        ):
+            cb.from_user.id not in sudousers):
             return await cb.answer(
                 "❎ You Are Not A Sudo User❗",
                 cache_time=0,
@@ -52,17 +44,14 @@ def cb_wrapper(func):
                 return await cb.answer(
                     f"❎ Something Went Wrong, Please Check Logs❗..."
                 )
-        
     return wrapper
-
 
 def inline_wrapper(func):
     from ... import __version__
     async def wrapper(bot, query):
         sudousers = SUDOERS
         if (query.from_user.id != app.me.id and
-            query.from_user.id not in sudousers
-        ):
+            query.from_user.id not in sudousers):
             try:
                 button = [
                     [
@@ -76,39 +65,33 @@ def inline_wrapper(func):
                     query.id,
                     cache_time=1,
                     results=[
-                        (
-                            InlineQueryResultPhoto(
-                                photo_url=f"https://files.catbox.moe/r58nec.jpg",
-                                title="🥀 Shukla Userbot ✨",
-                                thumb_url=f"https://files.catbox.moe/r58nec.jpg",
-                                description=f"🌷 Deploy Your Own SHUKLAUSERBOT🌿...",
-                                caption=f"<b>🥀 Welcome » To » Shukla 🌷\n✅ Userbot {__version__} ✨...</b>",
-                                reply_markup=InlineKeyboardMarkup(button),
-                            )
+                        InlineQueryResultPhoto(
+                            photo_url="https://files.catbox.moe/83d5lc.jpg",
+                            title="🥀 Shukla Userbot ✨",
+                            thumb_url="https://files.catbox.moe/83d5lc.jpg",
+                            description="🌷 Deploy Your Own SHUKLAUSERBOT🌿...",
+                            caption=f"<b>🥀 Welcome » To » Shukla 🌷\n✅ Userbot {__version__} ✨...</b>",
+                            reply_markup=InlineKeyboardMarkup(button),
                         )
                     ],
                 )
             except Exception as e:
-                print(str(e))
+                print(format_exc())
                 await bot.answer_inline_query(
                     query.id,
                     cache_time=1,
                     results=[
-                        (
-                            InlineQueryResultArticle(
-                                title="",
-                                input_message_content=InputTextMessageContent(
-                                    f"||**🥀 Please, Deploy Your Own Shukla Userbot❗...\n\nRepo:** <i>https://github.com/itzshukla/STRANGER-OPUSERBOT2.0/</i>||"
-                                ),
-                            )
+                        InlineQueryResultArticle(
+                            title="Deploy Your Own Shukla Userbot",
+                            input_message_content=InputTextMessageContent(
+                                f"**🥀 Please, Deploy Your Own Shukla Userbot❗...\n\nRepo:** [Click Here](https://github.com/itzshukla/STRANGER-OPUSERBOT2.0)"
+                            ),
                         )
                     ],
                 )
-            except Exception as e:
-                print(str(e))
-                pass
         else:
-           return await func(bot, query)
-
+            try:
+                return await func(bot, query)
+            except Exception as e:
+                print(format_exc())
     return wrapper
-
