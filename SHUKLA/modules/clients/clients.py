3798edb81e781d8532a0fca06ea7e677f0c4618c
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-13 17:31:12 +0530

    Update clients.py

--- SHUKLA/modules/clients/clients.py
+++ SHUKLA/modules/clients/clients.py
@@ -53,21 +53,14 @@ async_dirs()
     
 
 app = Client(
-    name = "SHUKLA",
+    name = "BADUC",
     api_id = API_ID,
     api_hash = API_HASH,
     session_string = STRING_SESSION,
 )
 
-ass = Client(
-    name = "ShuklaPlayer",
-    api_id = API_ID,
-    api_hash = API_HASH,
-    session_string = SESSION_STRING,
-)
-
 bot = Client(
-    name = "ShuklaSUPPORT",
+    name = "BADUCSUPPORT",
     api_id = API_ID,
     api_hash = API_HASH,
     bot_token = BOT_TOKEN,
@@ -82,7 +75,7 @@ def mongodbase():
         LOGGER.info("Connecting To Your Database ...")
         async_client = AsyncIOMotorClient
         mongobase = async_client(MONGO_DB_URL)
-        mongodb = mongobase.SHUKLA
+        mongodb = mongobase.BADUC
         LOGGER.info("Conected To Your Database.")
     except:
         LOGGER.error("Failed To Connect, Please Change Your Mongo Database !")
@@ -114,8 +107,6 @@ async def run_async_clients():
         await app.join_chat("SHIVANSH474")
     except:
         pass
-    if SESSION_STRING:
-        LOGGER.info("Starting Assistant ...")
         await ass.start()
         LOGGER.info("Assistant Started.")
         try:
@@ -123,8 +114,8 @@ async def run_async_clients():
         except:
             pass
         try:
-            await app.join_chat("MASTIWITHFRIENDSXD")
-            await app.join_chat("SHIVANSH474")
+            await app.join_chat("bad")
+            await app.join_chat("badmunda")
         except:
             pass
     LOGGER.info("Starting Helper Robot ...")
@@ -138,5 +129,3 @@ async def run_async_clients():
     await call.start()
     LOGGER.info("PyTgCalls Client Started.")
     await sudo_users()
-    
-    
